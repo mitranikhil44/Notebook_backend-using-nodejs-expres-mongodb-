8626eba29943a5b9378f98803408ed6b9c42b008
Nikhil Raj <69292853+mitranikhil44@users.noreply.github.com>
2022-12-13 22:34:03 +0530

    Initial commit

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const cors = require('cors');
 
 connectToMongo();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
+
+// Catch all handler for all other request.
+// app.use('*', (req, res) => {
+//   res.json({ msg: 'no route handler found' }).end()
+// })
+
+// Get a full listing
+app.get('/', async (req, res) => {
+  res.send("I - Notebook backed is now running");
+})
 
 app.listen(port, () => {
   console.log(`I - Notebook listening on port ${port}`);
